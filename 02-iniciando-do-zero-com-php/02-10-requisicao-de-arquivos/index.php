0bc1a5533755bf266a4776707cdb2c80c3cde094
alat <alat@pop-os.localdomain>
2020-05-22 10:09:26 -0300

    Aula assistida

--- 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -8,6 +8,37 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
 
+# include inclui arquivo mas não trava o programa caso o arquivo não exista
+#include "file.php";
+
+# Funciona pois ele procura no diretório desse código
+include "header.php";
+
+# Mas o ideal é usar a constande mágica ___DIR___
+include __DIR__."/header.php";
+
+$profile = new StdClass();
+$profile->nome = "André";
+$profile->cargo = "Administrador";
+
+include './profile.php';
+
+$profile = new StdClass();
+$profile->nome = "Kátia";
+$profile->cargo = "Apoio Acadêmico";
+
+include './profile.php';
+
+$profile = new StdClass();
+$profile->nome = "Matheus";
+$profile->cargo = "Analista de Sistemas";
+
+include_once './profile.php';   // O arquivo não é incluído pois ele jká foi incluído.
+# o include_once garante que o arquivo é incluído uma vez. Como ele já foi incluído antes
+# o php não faz nada.
+
+# Quando vc quer garantir que o arquivo será incluído somente uma vez. Tipo arquivo de configuração. 
+# Senão as constantes configuradas em seu arquivo serão reconfiguradas gerando um erro
 
 /*
  * [ require ] https://php.net/manual/pt_BR/function.require.php
@@ -15,3 +46,7 @@ fullStackPHPClassSession("include, include_once", __LINE__);
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
 
+# require trava o programa caso o arquivo não exista
+#require "file.php";
+
+require './profile.php';
---
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/profile.php
@@ -0,0 +1,11 @@
+<article style="
+         padding: 5px 20px;
+         background: #eee;
+">
+    <h1>
+        <?= $profile->nome ?>
+    </h1>
+    <p>
+        Tem o cargo de <?= $profile->cargo ?>
+    </p>
+</article>

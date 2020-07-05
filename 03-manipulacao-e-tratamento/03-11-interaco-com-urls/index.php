82f4c0f8c548f47519c0199d210962d2d5474486
andreAguia <alataguia@gmail.com>
2020-07-05 16:21:21 -0300

    Bug na estatística Bug no menu principal

--- 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
+++ 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
@@ -16,17 +16,27 @@ $data = [
     "company" => "Uenf",
 ];
 
-$arguments = http_build_query($data);
-echo "<p><a href='index.php?{$arguments}'>Args by Array</a></p>";
+$arguments1 = http_build_query($data);
+echo "<p><a href='index.php?{$arguments1}'>Args by Array</a></p>";
 
 var_dump($_GET);
 
-$object = (object) $data;
+$data2 = [
+    "name"    => "Arthur",
+    "company" => "Casa",
+];
+
+$object = (object) $data2;
+
+$arguments2 = http_build_query($object);
+echo "<p><a href='index.php?{$arguments2}'>Args by Object</a></p>";
 
 var_dump(
-        $arguments,
+        $arguments1,
+        $arguments2,
         $object,
         http_build_query($object),
+        $_GET,
 );
 
 /*
@@ -34,3 +44,30 @@ var_dump(
  * [ filter list ] https://php.net/manual/en/filter.filters.php
  */
 fullStackPHPClassSession("segurança", __LINE__);
+
+$dataFilter = http_build_query([
+   "name"=>"André",
+    "company"=>"Uenf",
+    "mail"=>"jujuba@gmail.com",
+    "site"=>"www.jujuba.com.br",
+    "script"=>"<script>alert('Isso é JavaScript')</script>",
+]);
+
+echo "<p><a href='index.php?{$dataFilter}'>DAta Filter</a></p>";
+
+$dateUrl = filter_input_array(INPUT_GET,FILTER_SANITIZE_STRIPPED);
+
+if($dateUrl){
+    if(in_array("", $dateUrl)){
+        echo "<p class='trigger warning'> Faltam Dados</p>";
+    }elseif(empty($dateUrl["mail"])){
+        echo "<p class='trigger warning'> Falta o e-mail</p>";
+    }elseif(!filter_var($dateUrl["mail"],FILTER_VALIDATE_EMAIL)){
+        echo "<p class='trigger warning'>E-mail Inválido</p>";
+    }else{
+        echo "<p class='trigger accept'>Tudo Certo !!</p>";
+    }
+}else{
+    var_dump(false);
+}
+var_dump($dateUrl);

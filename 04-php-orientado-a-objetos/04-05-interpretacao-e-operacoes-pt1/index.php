e46d2b4fa9813c5d0da3692b75413660ad7aa68b
andreAguia <alat@alat-pc>
2020-07-20 12:02:01 -0300

    Aula assitida

--- 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
@@ -11,16 +11,15 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("__construct", __LINE__);
 
+# A rotina __construct é executada toda vez
+# que é feito um operador new para um objeto
 $user = new Source\Interpretation\User(
         "André",
         "Luis",
-        "jujuba@gmail.com"
+//        "jujuba@gmail.com"
 );
 
 var_dump($user);
-
-
-
 
 /*
  * [ clone ] Executado automaticamente quando um novo objeto é criado a partir do operador clone.
@@ -28,9 +27,40 @@ var_dump($user);
  */
 fullStackPHPClassSession("__clone", __LINE__);
 
+# Copiando o objeto
+$andre = $user;
+
+# Quando se modifica $kaue, 
+# se modifica $andre também pois é valor por referência
+# é como se fosse $var = &$outroVar;
+$kaue = $andre;
+$kaue->setFirstName("Kaue");
+$kaue->setLastName("Cardoso");
+
+# PAra resolver esse problema temos a palavra
+# reservada clone
+$matheus = clone $andre;
+$matheus->setFirstName("Matheus");
+$matheus->setLastName("André");
+
+$gustavo = clone $kaue;
+
+var_dump(
+        $andre,
+        $kaue,
+        $matheus,
+        $gustavo
+);
 
 /*
  * [ destruct ] Executado automaticamente quando o objeto é finalizado
  * http://php.net/manual/pt_BR/language.oop5.decon.php
  */
 fullStackPHPClassSession("__destruct", __LINE__);
+
+# Os objetos SEMPRES são destruídos ao final da aplicação
+# Dessa forma podemos colocar rotinas que serão executadas
+# Nesse momento com a função __destruct
+
+# A destruição dos objetos pode ser feita manualmente com o comando
+$andre = null;
--- 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/source/Interpretation/User.php
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/source/Interpretation/User.php
@@ -2,27 +2,61 @@
 
 namespace Source\Interpretation;
 
-class User {
+class User
+{
 
     private $firstName;
     private $lastName;
     private $email;
 
-    public function __construct($firstName, $lastName, $email) {
+    public function __construct($firstName, $lastName = null, $email = null)
+    {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->email = $email;
     }
 
-    public function getFirstName() {
+    function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    function getFirstName()
+    {
         return $this->firstName;
     }
 
-    public function getLastName() {
+    function getLastName()
+    {
         return $this->lastName;
     }
 
-    public function getEmail() {
+    function getEmail()
+    {
         return $this->email;
     }
+
+    function __clone()
+    {
+        $this->firstName = null;
+        $this->lastName = null;
+        echo"<p class='trigger'>CLonou!</p>";
+    }
+
+    function __destruct()
+    {
+        var_dump($this);
+        echo"<p class='trigger accept'>O Objet foi destruído!</p>";
+    }
+
 }
